57f29650ebbe3e0224a6913c630ff29ef548eb71
Jon Dedman <jonathandedman@googlemail.com>
2023-10-07 16:11:27 +0100

    create square using python turtle module

---
+++ turtle/square/main.py
@@ -0,0 +1,37 @@
+from turtle import Turtle, Screen
+# import heroes
+
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("red")
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+
+# hero =  heroes.gen()
+# print(hero)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
