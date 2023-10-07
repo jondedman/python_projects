9568a5011893a092854f425bd785c14405d65003
Jon Dedman <jonathandedman@googlemail.com>
2023-10-07 16:17:05 +0100

    draw dashed line with turtle

---
+++ turtle/dashed.py
@@ -0,0 +1,17 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("red")
+
+for _ in range(15):
+    timmy.forward(10)
+    timmy.penup()
+    timmy.forward(10)
+    timmy.pendown()
+
+
+
+
+screen = Screen()
+screen.exitonclick()
