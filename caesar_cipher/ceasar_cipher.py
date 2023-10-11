dd263a059a5c2c532721dd37cf7724e1390bcde0
Jon Dedman <jonathandedman@googlemail.com>
2023-10-11 23:49:54 +0100

    refactor and use modulus to handle case where shift is greater than number of alphabet letters

--- caesar_cipher/ceasar_cipher.py
+++ caesar_cipher/ceasar_cipher.py
@@ -1,4 +1,8 @@
+import art
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+print(art.logo)
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
@@ -7,17 +11,14 @@ shift = int(input("Type the shift number:\n"))
 
 def caesar(text,shift,direction):
     result = ""
+    if direction == "decode":
+        shift *= -1
+        print(shift)
     for letter in text:
         position = alphabet.index(letter)
-        if direction == "encode":
-            new_position = position + shift
-        else:
-            new_position = position - shift
-        # handle the case where the new_position is greater or less than 25 to wrap around to the beginning of the alphabet to avoid an out of bounds error
-        if position + shift > 25:
-            new_position = position + shift - 26
-        elif position - shift < 0:
-            new_position = position - shift + 26
+        new_position = position + shift
+        if new_position > 25 or new_position < 0:
+            new_position = new_position % 25
         new_letter = alphabet[new_position]
         result += new_letter
     print(f"The text is {result}")
