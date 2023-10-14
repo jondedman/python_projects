24d1ae1cb788e4bc6a6201e7b9e58a14c6674f2b
Jon Dedman <jonathandedman@googlemail.com>
2023-10-14 11:18:53 +0100

    refactor

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -60,62 +60,91 @@ import random
 
 #Hint 14: Ask the user if they want to restart the game. If they answer yes, clear the console and start a new game of blackjack and show the logo from art.py.
 
-deck = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+def calculate_score(cards):
+  if len(cards) == 2 and sum(cards) == 21:
+      return 0
+  elif any(card == 11 for card in cards) and sum(player_cards) > 21:
+      cards.remove(11)
+      cards.append(1)
+      return sum(cards)
+  else:
+      return sum(cards)
 
-player_cards = []
-dealer_cards = []
+def compare_scores(player_total, dealer_total):
+    if player_total == 0:
+        print("You have won with a blackjack!")
+    elif dealer_total == 0:
+        print("The dealer has won with a blackjack!")
+    elif player_total > dealer_total:
+        print(f"You win! your total is {player_total} vs the dealer {dealer_total}")
+    elif dealer_total > player_total:
+        print(f"You have lost. The dealer has {dealer_total} vs your total of: {player_total}")
+    else:
+        print(f"It is a draw! You both have {player_total}")
 
-def deal(player_cards, dealer_cards):
-    game = []
-    player_cards.append(random.choice(deck))
-    dealer_cards.append(random.choice(deck))
-    game.append(player_cards)
-    game.append(dealer_cards)
-    if sum(player_cards)  > 21:
-        print("You are bust!")
-        return
-    elif sum(dealer_cards) > 21:
-        print("Dealer is bust - you win")
-        return
-    return game
 
 def hit(cards):
     cards.append(random.choice(deck))
-    game.append(cards)
+    return cards
+
+def deal(player_cards, dealer_cards):
+    game = [player_cards, dealer_cards]
+    print("Dealing first card...")
+    player_cards = hit(player_cards)
+    dealer_cards = hit(dealer_cards)
+    print(f"The dealer's hand is {dealer_cards} and their current score is {sum(dealer_cards)}")
+    print(f"Your hand is {player_cards} and your current score is {sum(player_cards)}")
+    print("Dealing second card...")
+    player_cards = hit(player_cards)
+    dealer_cards = hit(dealer_cards)
+    print(f"The dealer's hand is {dealer_cards[0]} X")
+    print(f"Your hand is {player_cards} and your current score is {sum(player_cards)}")
     return game
 
-
-game = deal(player_cards, dealer_cards)
-
-print(f"The dealer's hand is {game[1]}")
-print(f"Your hand is {game[0]}")
-print("Dealing second card...")
-
-deal(player_cards, dealer_cards)
-print(f"The dealer's hand is {game[1][0]} X")
-print(f"Your hand is {game[0]}")
-
-twist = True
-
-while twist:
-    player_total = sum(player_cards)
-    dealer_total = sum(dealer_cards)
-    choice = input("Would you like another card? Type Y or N ").lower()
+def play():
+    choice = input("Would you like to play blackjack? Type Y or N ").lower()
     if choice == "y":
-        hit(player_cards)
-        print(f"The dealer's hand is {game[1][0]} X")
-        print(f"Your hand is {game[0]}")
+        return True
     else:
-        dealer_choice = random.choice([True, False])
-        while sum(dealer_cards) < 21 and dealer_choice == True:
-            print("dealer draws another")
-            hit(dealer_cards)
-            random.choice(bool)
-        print("Dealer stands")
-        if player_total > dealer_total:
-            print(f"You win! your total is {player_total} vs the dealer {dealer_total}")
-        elif dealer_total > player_total:
-            print(f"You have lost. The dealer has {dealer_total} vs your total of: {player_total}")
+        return False
+
+while play():
+    deck = [11,2,3,4,5,6,7,8,9,10,10,10,10]
+    player_cards = []
+    dealer_cards = []
+    player_total = 0
+    dealer_total = 0
+    game = deal(player_cards, dealer_cards)
+
+    player_cards = game[0]
+    dealer_cards = game[1]
+
+    twist = True
+
+    while twist:
+        player_total = sum(player_cards)
+        dealer_total = sum(dealer_cards)
+        choice = input("Would you like another card? Type Y or N ").lower()
+        if choice == "y":
+            hit(player_cards)
+            player_total = calculate_score(player_cards)
+            print(f"The dealer's hand is {dealer_cards[0]} X")
+            print(f"Your hand is {player_cards} and your current score is {player_total}")
+            if player_total > 21:
+                print("You have gone bust!")
+                twist = False
+            elif player_total == 0:
+                print("You have won with a blackjack!")
+                twist = False
         else:
-            print(f"It is a draw! You both have {player_total}")
-        twist = False
+            dealer_choice = random.choice([True, False])
+            while sum(dealer_cards) < 21 and dealer_choice == True:
+                print("dealer draws another")
+                hit(dealer_cards)
+                dealer_total = calculate_score(dealer_cards)
+                dealer_choice = random.choice([True, False])
+            print("Dealer stands")
+            compare_scores(player_total, dealer_total)
+            twist = False
+
+    print("Game over")
