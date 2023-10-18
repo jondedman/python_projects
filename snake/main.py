39a1bc9cfca70fe5a4a50155f66790087189975d
Jon Dedman <jonathandedman@googlemail.com>
2023-10-18 17:27:10 +0100

    implement snake game

--- snake/main.py
+++ snake/main.py
@@ -1,23 +1,22 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 from random import randint
 from random import choice
 import time
 
-screen = Screen()
-screen.setup(width=600, height=600)
-screen.bgcolor("black")
-screen.title("Snake Game")
-screen.tracer(0)
+def play():
+    screen = Screen()
+    screen.setup(width=600, height=600)
+    screen.bgcolor("black")
+    screen.title("Snake Game")
+    screen.tracer(0)
 
-snake = Snake()
+    snake = Snake()
+    food = Food()
+    scoreboard = Scoreboard()
 
-game_is_on = True
-
-while game_is_on:
-    screen.update()
-    time.sleep(0.1)
-    snake.move()
 
     screen.listen()
 
@@ -26,13 +25,36 @@ while game_is_on:
     screen.onkey(snake.left, "Left")
     screen.onkey(snake.right, "Right")
 
+    game_is_on = True
 
+    while game_is_on:
+        screen.update()
+        time.sleep(0.1)
+        snake.move()
 
+    # Detect collision with food.
+        if snake.head.distance(food) < 15:
+            food.refresh()
+            snake.extend()
+            scoreboard.increase_score()
 
+    # Detect collision with wall.
+        if snake.head.xcor() > 280 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
+            game_is_on = False
+            scoreboard.game_over()
 
+    # Detect collision with tail.
+        for segment in snake.segments[1:]:
+            # if segment == snake.head:
+            #     pass
+            if snake.head.distance(segment) < 10:
+                game_is_on = False
+                scoreboard.game_over()
 
+    start_over = screen.textinput(title="Start Over", prompt="Do you want to start over? Type 'yes' or 'no'").lower()
+    if start_over == "yes":
+        screen.reset()
+        play()
+    screen.exitonclick()
 
-
-
-
-screen.exitonclick()
+play()
--- snake/snake.py
+++ snake/snake.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
 UP = 90
@@ -7,6 +8,7 @@ LEFT = 180
 RIGHT = 0
 
 class Snake:
+
     def __init__(self):
         self.segments = []
         self.create_snake()
@@ -19,6 +21,17 @@ class Snake:
             segment.penup()
             segment.goto(position)
             self.segments.append(segment)
+
+    def add_segment(self, position):
+        new_segment = Turtle(shape="square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
