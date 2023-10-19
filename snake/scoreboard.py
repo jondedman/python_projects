23ad2932777d211a68ce4193f76de25f32ad3358
Jon Dedman <jonathandedman@googlemail.com>
2023-10-19 23:36:32 +0100

    make high score in snake writable/readable to a file

--- snake/scoreboard.py
+++ snake/scoreboard.py
@@ -1,13 +1,16 @@
 from turtle import Turtle
 AlIGMENT = "center"
 FONT = ("Courier", 24, "normal")
+with open("data.txt") as file:
+            HIGH_SCORE= int(file.read())
+
 
 class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        self.high_score = HIGH_SCORE
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -25,6 +28,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", "w") as file:
+                file.write(str(self.high_score))
         self.score = 0
         self.update_scoreboard()
 
