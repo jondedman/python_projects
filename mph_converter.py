45d1965370d0f361e2ae43a58a53dbf81dc99b49
Jon Dedman <jonathandedman@googlemail.com>
2023-10-23 00:37:42 +0100

    implement converter

--- mph_converter.py
+++ mph_converter.py
@@ -4,7 +4,13 @@ window = Tk()
 window.title("Miles to Km Converter")
 window.config(padx=20, pady=20)
 
-button = Button(text="Calculate")
+def miles_to_km():
+    miles = float(input.get())
+    result = round(miles * 1.609)
+    km_output["text"] = result
+
+
+button = Button(text="Calculate", command=miles_to_km)
 button.grid(column=1,row=3)
 input = Entry(width=5)
 input.grid(column=1, row=1)
@@ -16,6 +22,11 @@ km_output = Label(text=0)
 km_output.grid(column=1, row=2)
 miles = Label(text="Miles")
 miles.grid(column=2, row=1)
+
+
+
+
+
 
 
 window.mainloop()
