8eb6dd9ca5df364b32cfbe61609ab06f6b0aeb79
Jon Dedman <jonathandedman@googlemail.com>
2023-11-08 23:02:48 +0000

    complete flashcard app

--- flash-card-project-start/main.py
+++ flash-card-project-start/main.py
@@ -1,11 +1,49 @@
 from tkinter import *
 import os
 from tkinter import messagebox
-from PIL import Image, ImageTk
+import pandas as pd
+import random
+
 
 
 BACKGROUND_COLOR = "#B1DDC6"
 
+# open csv
+try:
+    df = pd.read_csv('./data/words_to_learn.csv')
+except FileNotFoundError:
+    df = pd.read_csv('./data/french_words.csv')
+
+to_learn = df.to_dict(orient='records')
+
+# french_words = list(df.French)
+
+def next_card():
+    global current_card
+    global flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    new_word = current_card["French"]
+    canvas.itemconfig(title, text="French", fill="black")
+    canvas.itemconfig(word, text=new_word, fill="black")
+    canvas.itemconfig(pic, image=card_front)
+    flip_timer = window.after(3000, flip)
+
+
+
+def correct():
+    to_learn.remove(current_card)
+    df = pd.DataFrame(to_learn)
+    df.to_csv('./data/words_to_learn.csv', index=False)
+    next_card()
+
+
+
+def flip():
+    flip_word = current_card["English"]
+    canvas.itemconfig(pic, image=card_back)
+    canvas.itemconfig(title, text="English", fill="white")
+    canvas.itemconfig(word, text=flip_word, fill="white")
 
 
 # ---------------------------- CREATE WINDOW ------------------------------- #
@@ -29,29 +67,32 @@ canvas.config(background=BACKGROUND_COLOR)
 
 # image set up
 card_front = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=card_front)
+card_back =  PhotoImage(file="images/card_back.png")
+pic = canvas.create_image(400, 263, image=card_front)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
 
-canvas.create_text(400, 50, text="Title", font=("Arial", 40, "italic"))
-canvas.create_text(400, 263, text="Word", font=("Arial", 60, "bold"))
+title = canvas.create_text(400, 50, text="French", font=("Arial", 40, "italic"))
+word = canvas.create_text(400, 263, text="", font=("Arial", 60, "bold"))
+
 
 # Buttons
 wrong_image = PhotoImage(file="images/wrong.png")
-wrong_button = Button(image=wrong_image, highlightthickness=0)
+wrong_button = Button(image=wrong_image, highlightthickness=0, command=next_card)
 wrong_button.grid(column=0, row=1)
 
 right_image = PhotoImage(file="images/right.png")
-right_button = Button(image=right_image, highlightthickness=0)
+right_button = Button(image=right_image, highlightthickness=0, command=correct)
 right_button.grid(column=1, row=1)
 
 
-
-
-
-
 canvas.grid(column=0, row=0, columnspan=2)
+
+flip_timer = window.after(3000, flip)
+next_card()
+
+
 
 
 
