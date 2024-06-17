8776be550813b0ea18fc2c10ed5203f8ab289811
Jon Dedman <jonathandedman@googlemail.com>
2024-06-17 23:54:16 +0100

    populate blog from API. will later be from a database

--- blog/main.py
+++ blog/main.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template
+import requests
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    r = requests.get("https://api.npoint.io/c790b4d5cab58020d391")
+    data = r.json()
+    print(data)
+
+
+    return render_template("index.html", data=data)
 
 if __name__ == "__main__":
     app.run(debug=True)
